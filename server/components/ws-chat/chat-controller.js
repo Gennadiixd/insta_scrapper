da0630a463a0d156f58d2e0ef601fd0379470684
gennadii <gennadiixd@gmail.com>
2020-02-27 22:40:35 +0300

    websockets in progress

--- server/components/ws-chat/chat-controller.js
+++ server/components/ws-chat/chat-controller.js
@@ -1,38 +1,22 @@
-const connections = new Set();
+// const connections = new Set();
+const connections = {};
 const jwt = require('express-jwt');
 
-// exports.requireJwt = expressJwt({
-//   secret: process.env.JWT_SECRET,
-//   userProperty: "auth"
-// });
-
-// app.use(jwt({
-//   secret: 'hello world !',
-//   credentialsRequired: false,
-//   getToken: function fromHeaderOrQuerystring(req) {
-//     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-//       return req.headers.authorization.split(' ')[1];
-//     } else if (req.query && req.query.token) {
-//       return req.query.token;
-//     }
-//     return null;
-//   }
-// }));
+const jwtParams = {
+  secret: process.env.JWT_SECRET,
+  userProperty: "auth",
+  getToken: (req) => req.cookies.t
+};
 
 const wsHandler = (ws, req) => {
-  connections.add(ws);
-  console.log('=========================================');
-  console.log('\x1b[36m', req.cookies.t);
-  
-  ws.on('connection', () => {
-    console.log('\x1b[36m', 'connected');
+
+  jwt(jwtParams)(req, {}, (err) => {
+    if (err) return;
+    connections[req.auth.userId] = ws;
   });
 
   ws.on('message', (message) => {
-    jwt({
-      secret: process.env.JWT_SECRET,
-      getToken: () => req.cookies.t
-    })(req, {}, (err) => console.log('\x1b[36m', err))
+    connections[req.auth.userId].send(message);
   });
 
   ws.on('close', () => {
